4be9d76a2a9660fd276855b62ba9ca73d6508d44
Paweł Fiszer <44177078+pfiszer@users.noreply.github.com>
2023-03-12 03:53:31 +0000

    Fixed multiprocessing

--- README.md
+++ README.md
@@ -1,12 +1,10 @@
 # UDP-Packet-Manager
 
-
 ## Overview
 
 This is a simple UDP packet manager to redirect UDP packets to different ports and ips.
 
 ## Config file
-
 
 After the first run of the app, the config file will be created.
 
@@ -21,10 +19,13 @@ localhost
 #Use template: port_from>port_to
 8080>80
 ```
+
 ---
+
 In order to send traffic to a specific ip, add it under the `\~~~SEND IPS~~~` section.
 
 In order to allow traffic from one port to another add them to the `\~~~MAP PORTS~~~` section using the template below.
 
 `from>to`
 
+**NOTE:** You cannot send to a port that is being used by this application
--- main.py
+++ main.py
@@ -1,33 +1,35 @@
 import socket
-from multiprocessing import Process
+from multiprocessing import Process, freeze_support
+freeze_support()
 
 
 def UDPSocket(sockport):
+    # Create a socket
     sock = socket.socket(
         socket.AF_INET, socket.SOCK_DGRAM, socket.IPPROTO_UDP)
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("0.0.0.0", sockport))
-    print(f"Port: {sockport} is active.")
+    print(f"Port: {sockport} is active.", flush=True)
     while True:
         try:
             message, _ = sock.recvfrom(2048)
-            print(message)
             for ip in send_ips:
                 for port in map_ports[sockport]:
                     sock.sendto(message, (ip, port))
         except Exception as e:
-            print(e)
+            pass
 
 
-""" LOAD CONFIG """
+if __name__ == '__main__':
+    """ LOAD CONFIG """
 
-CONFIG_FILE = []
-try:
-    with open("./UDPSplitter.cfg", 'r') as f:
-        CONFIG_FILE = f.readlines()
-except FileNotFoundError:
-    with open("./UDPSplitter.cfg", 'w+') as f:
-        f.write("""#Always receives at localhost
+    CONFIG_FILE = []
+    try:
+        with open("./UDPSplitter.cfg", 'r') as f:
+            CONFIG_FILE = f.readlines()
+    except FileNotFoundError:
+        with open("./UDPSplitter.cfg", 'w+') as f:
+            f.write("""#Always receives at localhost
 ~~~SEND IPS~~~
 #Add ip addresses where to send traffic
 #DO NOT add quotes or any unrelated characters
@@ -36,32 +38,35 @@ localhost
 #Add port mappings for the program
 #Use template: port_from>port_to
 8080>80""")
-    quit()
+        quit()
 
-send_ips = []
-map_ports = dict()
-processess = []
+    send_ips = []
+    map_ports = dict()
+    processess = []
 
-stage = None
-for line in CONFIG_FILE:
-    line = line.replace('\n', "")
-    if line.startswith("#"):
-        continue
-    elif line == "~~~SEND IPS~~~":
-        stage = "sendIP"
-    elif line == "~~~MAP PORTS~~~":
-        stage = "mapPORT"
-    else:
-        match stage:
-            case "sendIP":
-                send_ips.append(line)
-            case "mapPORT":
-                fromPort, toPort = map(int, line.split('>'))
-                try:
-                    map_ports[fromPort].append(toPort)
-                except:
-                    map_ports[fromPort] = [toPort]
+    stage = None
+    for line in CONFIG_FILE:
+        line = line.replace('\n', "")
+        if line.startswith("#"):
+            continue
+        elif line == "~~~SEND IPS~~~":
+            stage = "sendIP"
+        elif line == "~~~MAP PORTS~~~":
+            stage = "mapPORT"
+        else:
+            match stage:
+                case "sendIP":
+                    send_ips.append(line)
+                case "mapPORT":
+                    fromPort, toPort = map(int, line.split('>'))
+                    try:
+                        map_ports[fromPort].append(toPort)
+                    except:
+                        map_ports[fromPort] = [toPort]
 
-for port in map_ports:
-    processess.append(Process(target=UDPSocket, args=[port], daemon=True))
-    processess[-1].run()
+    for port in map_ports:
+        processess.append(Process(target=UDPSocket, args=[port], daemon=True))
+        processess[-1].start()
+
+    for process in processess:
+        process.join()
