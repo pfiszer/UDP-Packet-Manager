abb186264e1864313212cf2b0904ea97dcbbc0d1
Paweł Fiszer <44177078+pfiszer@users.noreply.github.com>
2023-04-23 23:02:12 +0100

    logging added

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from multiprocessing import Process, freeze_support, Manager
 import socket
+import logging
 
 
 def UDPSocket(sockport, map_ports):
@@ -10,16 +11,19 @@ def UDPSocket(sockport, map_ports):
     sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
     sock.bind(("0.0.0.0", sockport))
     print(f"Port: {sockport} is active.", flush=True)
+    logging(f"Port: {sockport} is active.")
     while True:
         try:
             message, _ = sock.recvfrom(2048)
             for ip in map_ports[sockport]:
                 for port in map_ports[sockport][ip]:
                     sock.sendto(message, (ip, port))
+                    logging.debug(f"Message from {sockport} to {ip}:{port}")
         except WindowsError:
             pass
         except Exception as e:
             print(e, flush=True)
+            logging.error(e)
 
 
 def convertConfig(port_cfg: str):
@@ -35,8 +39,11 @@ def clearIP(ip: str):
     for port in map_ports:
         try:
             delDict = map_ports[port]
+            logging.debug("Loaded port %s", port)
             del delDict[ip]
+            logging.debug(f"Deleted {ip} from {port}")
             map_ports[port] = delDict
+            logging.debug(f"New dict for {port}: {delDict}")
         except KeyError:
             pass
 
@@ -46,24 +53,32 @@ def mapPort(fromPort: int, ip: str | None, toPort: int | None):
 
     try:
         sharedDict = map_ports[fromPort]
+        logging.debug(f"Port {fromPort} extracted")
     except KeyError:
         map_ports[fromPort] = dict()
         sharedDict = map_ports[fromPort]
+        logging.debug(f"Port {fromPort} extracted after error")
     if ip == None or toPort == None:
         return
 
     try:
         sharedDict[ip] += [toPort]
+        logging.debug(f"IP {ip} and port {toPort} added to {fromPort}")
     except KeyError:
         try:
             sharedDict[ip] = []
             sharedDict[ip] += [toPort]
+            logging.debug(
+                f"IP {ip} and port {toPort} added to {fromPort} after 1 error")
         except KeyError:
             sharedDict = dict()
             sharedDict[ip] = []
             sharedDict[ip] += [toPort]
+            logging.debug(
+                f"IP {ip} and port {toPort} added to {fromPort} after 2 errors")
     finally:
         map_ports[fromPort] = sharedDict
+        logging.debug(f"{fromPort} updated with {sharedDict}")
 
 
 freeze_support()
@@ -76,6 +91,8 @@ if __name__ == '__main__':
     incomingPort = 58212
     dynamicConfig = False
     CONFIG_FILE = []
+    logging.basicConfig(filename='latest.log',
+                        encoding='utf-8', level=logging.DEBUG, filemode="a", format='[%(asctime)s] %(message)s', datefmt='%I:%M:%S')
     try:
         with open("./UDPSplitter.cfg", 'r') as f:
             CONFIG_FILE = f.readlines()
@@ -120,12 +137,14 @@ if __name__ == '__main__':
             except:
                 continue
             mapPort(fromPort, toIP, toPort)
+    logging.info("CONFIG LOADED")
     ''' START PROGRAM '''
     for port in map_ports:
         processess.append(Process(target=UDPSocket, args=[
                           port, map_ports], daemon=True))
         processess[-1].start()
-
+        logging.debug(f"Process for port {port} started.")
+    logging.info("Listening processes started.")
     if dynamicConfig:
         ''' Listening for incoming config changes'''
         sock = socket.socket(
@@ -133,22 +152,28 @@ if __name__ == '__main__':
         sock.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1)
         sock.bind(("0.0.0.0", incomingPort))
         print(f"Listening for config on port: {incomingPort}.", flush=True)
+        logging.info(f"Dynamic config started at port {incomingPort}.")
         while True:
             try:
                 message, addr = sock.recvfrom(16)
                 ip = addr[0]
                 ports = message.decode('utf-8').split('>')
+                logging.debug(f"Message: {message} from address {addr}")
                 if len(ports) > 1:
                     inPort, outPort = map(int, ports)
                 elif ports[0] == "clear":
                     clearIP(ip)
                     print(f"IP address {ip} has been cleared")
+                    logging.debug(f"IP address {ip} has been cleared")
                     continue
                 else:
                     inPort = outPort = int(ports[0])
                 mapPort(inPort, ip, outPort)
+                logging.info(f"Port {inPort} mapped to {ip}:{outPort}")
+                logging.debug(f"Current dict state: {map_ports}")
             except Exception as e:
                 print(e)
+                logging.error(e)
 
     for process in processess:
         process.join()
